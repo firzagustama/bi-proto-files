07c3bc451362a08bd2d9b5ab421914c720a6b756
Muhammad Firza Gustama <mfirza_gustama@bi.go.id>
2023-01-27 15:45:03 +0700

    init

---
+++ deploy_nexus.ps1
@@ -0,0 +1,20 @@
+﻿$version = Read-Host -Prompt "Package version"
+
+#java
+$mvn_version = "-Dversion=${version}"
+
+cd .\java
+mvn clean deploy $mvn_version
+cd ..
+
+#dotnet
+$nuget_url = "http://localhost:8081/repository/nuget-hosted/"
+$nuget_api_key = "6e87ef10-5c63-3366-bc47-b48a3df687be"
+
+cd .\dotnet
+dotnet pack -c Release -o .\nupkg -p:Version=${version}
+cd .\nupkg
+dotnet nuget push corp.bi.go.id.rpc.interface.${version}.nupkg --api-key $nuget_api_key --source $nuget_url
+cd ..\..
+
+Read-Host -Prompt "Press enter to exit"
